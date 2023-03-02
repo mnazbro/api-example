ff9856eef89b40d2c645faca85b1f43a27914202
dsinghvi <deep@usebirch.com>
2023-03-02 06:07:38 -0800

    gitignore generated code

--- backend/src/fern_fastapi_starter/movies_service.py
+++ backend/src/fern_fastapi_starter/movies_service.py
@@ -1,11 +1,8 @@
+from fern_fastapi_starter.api.generated.resources.imdb.types.genre import Genre
 from .api.generated.resources.imdb.service.service import AbstractImdbService
 from .api.generated import Movie, MovieId
 
 
 class MoviesService(AbstractImdbService):
     def get_movie(self, *, movie_id: str) -> Movie:
-        return Movie(
-            title="Goodwill Hunting",
-            rating=9.8,
-            id=MovieId.from_str("tt0119217"),
-        )
+        return Movie(title="Goodwill Hunting", rating=9.8, id=MovieId.from_str("tt0119217"), genre=Genre.COMEDY)
